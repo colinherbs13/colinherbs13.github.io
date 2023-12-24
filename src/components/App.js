8e0b2f7d1c7baf2650687b81950227f1bfc1ffab
Colin Herbert <colin.herbert.13@gmail.com>
2023-12-23 21:03:24 -0700

    [Commit 4] Created state machine for various react pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,44 @@
+import React from 'react';
 import logo from '../logo.svg';
 import '../stylesheets/App.css';
+import Home from '../pages/Home.js'
+import Projects from '../pages/Projects.js'
+import AboutMe from '../pages/AboutMe.js'
+import Contact from '../pages/Contact.js'
+
 
 function App() {
+  function homeRoute(){
+    setRouter(0);
+  }
+
+  function projectsRoute(){
+    setRouter(1);
+  }
+
+  function aboutRoute(){
+    setRouter(2);
+  }
+
+  function contactRoute(){
+    setRouter(3);
+  }
+
+  const [router, setRouter] = React.useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <button onClick={homeRoute}>Home</button>
+        <button onClick={projectsRoute}>Projects</button>
+        <button onClick={aboutRoute}>About Me</button>
+        <button onClick={contactRoute}>Contact</button>
+      </nav>
+      
+      { router===0 ? <Home/> : 
+      router===1 ? <Projects/> :
+      router===2 ? <AboutMe/> :
+      router===3 ? <Contact/> : 
+      null }
     </div>
   );
 }
